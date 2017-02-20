6724c31c6a91020ae632753d01ba62799390dba6
peterittner <peter.ittner@gmail.com>
2017-02-20 12:56:32 +0100

    adds global exception handling

--- ExcelToHtmlConverter/App.xaml.cs
+++ ExcelToHtmlConverter/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using ExcelToHtmlConverter.Common;
+using System;
+using System.IO;
+using System.Text;
+using System.Windows;
 
 namespace ExcelToHtmlConverter
 {
@@ -7,5 +11,38 @@ namespace ExcelToHtmlConverter
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var filename = string.Empty;
+            if (e.ExceptionObject is Exception)
+            {
+                filename = new ExceptionHandler().WriteToLog(e.ExceptionObject as Exception);
+            }
+            else
+            {
+                filename = new ExceptionHandler().WriteToLog("An unexpected error occurred without any further information.");
+            }
+            ShowErrorMessage(filename);
+            Environment.Exit(1);
+        }
+
+        private void ShowErrorMessage(string filename)
+        {
+            MessageBox.Show(new StringBuilder()
+                .AppendLine("An error occurred.")
+                .AppendLine("Please see the following logfile for more information.")
+                .AppendLine(filename)
+                .AppendLine()
+                .AppendLine("The application will now close...")
+                .ToString()
+                , "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
 }
---
+++ ExcelToHtmlConverter/Common/ExceptionHandler.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelToHtmlConverter.Common
+{
+    public class ExceptionHandler
+    {
+        private const string LINE_SEPEARTOR = "====================================================";
+
+        public string WriteToLog(Exception e)
+        {
+            var builder = new StringBuilder();
+            builder
+                .AppendLine("An Error occurred in ExcelToHTMLConverter!")
+                .AppendLine("TIM: " + DateTime.Now)
+                .AppendLine("USR: " + Environment.UserName)
+                .AppendLine("PWD: " + Environment.CurrentDirectory)
+                .AppendLine();
+
+            if (e != null)
+            {
+                if (!string.IsNullOrEmpty(e.Message) && !string.IsNullOrEmpty(e.StackTrace))
+                {
+                    builder
+                        .AppendLine(e.Message)
+                        .AppendLine(LINE_SEPEARTOR)
+                        .AppendLine("StackTrace:")
+                        .AppendLine(e.StackTrace)
+                        .AppendLine(LINE_SEPEARTOR);
+                }
+                Exception inner = e.InnerException;
+                while (inner != null)
+                {
+                    builder
+                        .AppendLine()
+                        .AppendLine("Inner Exception:")
+                        .AppendLine();
+                    if (!string.IsNullOrEmpty(inner.Message) && !string.IsNullOrEmpty(inner.StackTrace))
+                    {
+                        builder
+                            .AppendLine(inner.Message)
+                            .AppendLine(LINE_SEPEARTOR)
+                            .AppendLine("StackTrace of Inner Exception:")
+                            .AppendLine(inner.StackTrace)
+                            .AppendLine(LINE_SEPEARTOR);
+                    }
+                    inner = inner.InnerException;
+                }
+            }
+            return WriteToLog(builder.ToString());
+        }
+
+        public string WriteToLog(string s)
+        {
+            var filename = GetFilename();
+            File.WriteAllText(filename, s);
+            return filename;
+        }
+
+        public string GetFilename()
+        {
+            var path = Path.GetTempPath();
+            var prefix = "ExcelToHtmlConverter_";
+            var timestamp = DateTime.Now
+                .ToString("yyyy.MM.dd_HH_mm_ss__ffffzzz")
+                .Replace("+", "P")
+                .Replace(":", "_");
+            var extension = ".txt";
+            return path + prefix + timestamp + extension;
+        }
+    }
+}
